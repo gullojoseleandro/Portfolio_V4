a7f96b9e0bbb70ec7e6656acc8fa258b3ccb2c60
leandrosec19 <leandrosec19@gmail.com>
2024-06-26 23:34:15 +0000

    marging litte change

--- src/Components/Cards/jsx/CVCards.jsx
+++ src/Components/Cards/jsx/CVCards.jsx
@@ -3,7 +3,7 @@ import '../css/CVCards.css'
 const CVCards = ({title, company, date, content}) => {
     return (
         <>
-            <div className="my-3">
+            <div className="my-2">
                 <h3 className="cardtitle fw-bold">{title}</h3>
                 <h6 className="cardcompany fw-bold">{company} | {date}</h6>
                 <div className='cardcontent'>{content}</div>
