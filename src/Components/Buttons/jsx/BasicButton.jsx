c1f1d2b92bc1648975c8d6ddc29f663c31b6a8c2
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-15 20:49:37 -0300

    update: BasicButton general component changed

--- src/Components/Buttons/jsx/BasicButton.jsx
+++ src/Components/Buttons/jsx/BasicButton.jsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function BasicButtons({ Contained, onclick, href, buttonclass }) {
+export default function BasicButtons({ content, onClick, href, buttonclass, buttonstyle }) {
   return (
     <Stack spacing={2} direction="row" className={buttonclass}>
-      <Button variant="contained" onClick={onclick} href={href}>{Contained}</Button>
+      <Button style={buttonstyle} onClick={onClick} href={href}>{content}</Button>
     </Stack>
   );
 }
