dae6b22c54a6d340a751baffa6fbf665b640875d
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-20 17:41:39 -0300

    Create CVButton.jsx

--- src/Pages/PortfolioPage/Components/Button/CVButton.jsx
+++ src/Pages/PortfolioPage/Components/Button/CVButton.jsx
@@ -1,23 +1,13 @@
 import React, { useState } from "react";
 import CV from "./../../../../assets/Files/Gullo José Leandro - Octubre 2024.pdf";
+import "./CVButton.css"; 
 
 const CVButton = () => {
     const [activeButton, setActiveButton] = useState(false);
 
     return (
         <button
-            style={{
-                backgroundColor: activeButton ? "rgb(190, 38, 33)" : "#FFBA08",
-                color: activeButton ? "#FFBA08" : "#03071E",
-                fontSize: "0.875rem",
-                padding: "10px 20px", 
-                borderRadius: "30px", 
-                transition: "background-color 0.4s, transform 0.4s, box-shadow 0.4s",
-                transform: activeButton ? "scale(1.1)" : "scale(1)", 
-                boxShadow: "0 0 7rem #FFBA08",
-                outline: "none", 
-            }}
-            className="btn border border-none fw-bold cursor-pointer"
+            className={`cv-button ${activeButton ? 'active' : ''}`}
             onMouseEnter={() => setActiveButton(true)}
             onMouseLeave={() => setActiveButton(false)}
         >
@@ -25,8 +15,8 @@ const CVButton = () => {
                 href={CV} 
                 target="_blank"
                 rel="noreferrer"
-                style={{ textDecoration: "none", color: "inherit"}}>
-                <p className={"p-0 m-0"}>Descargá mi CV</p>
+                style={{ textDecoration: "none", color: "inherit" }}>
+                <p className="cv-button-text">Descargá mi CV</p>
             </a>
         </button>
     );
