b229cd266030d745bf6fb63c1108ecf6cf92261e
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-20 17:43:30 -0300

    Update HomeSectionContent.jsx

--- src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
+++ src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
@@ -1,18 +1,19 @@
+import React, { memo } from "react";
 import SocialMenu from "../../../Components/SocialMenu/SocialMenu";
 import Title from "../Components/Title/Title";
 import CVButton from "../Components/Button/CVButton";
 
-const HomeSectionContent = () => {
+const HomeSectionContent = memo(() => {
     return (
         <section
             className={"d-flex flex-column h-100 align-items-center justify-content-center text-center"}
         >
-            <SocialMenu />
-            <Title />
-            <CVButton />
+            <div data-aos="fade-down"><SocialMenu /></div>
+            <div data-aos="zoom-in"><Title /></div>
+            <div data-aos="fade-up"><CVButton /></div>
+            
         </section>
-    )
-
-}
+    );
+});
 
 export default HomeSectionContent;
