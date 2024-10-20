f535c87f27948353079c7e72fc7b13539c8f2a63
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-20 06:43:19 -0300

    Create Title.jsx

---
+++ src/Pages/PortfolioPage/Components/Title/Title.jsx
@@ -0,0 +1,14 @@
+const Title = () => {
+    return (
+        <>
+            <h1 className="mt-3 m-0 p-0" style={{ fontSize: "5.5rem", color: "whitesmoke" }}>José Leandro <span style={{ color: "#FFBA08" }}>Gullo</span></h1>
+            <p style={{ fontSize: "1.1rem", color: "whitesmoke" }} className={"m-0 p-0"}>Desarrollador Full<span style={{ color: "#FFBA08" }}>_</span>Stack</p>
+            <hr
+                style={{ backgroundColor: "#FFBA08", height: "2px", width: "150px" }}
+                className={"rounded-5 border border-none m-0 p-0 mb-6"}
+            />
+        </>
+    )
+}
+
+export default Title;
