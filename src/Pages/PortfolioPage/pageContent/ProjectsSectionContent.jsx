53597d4313f10c31b331b60b25850087fafe23ba
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-20 09:10:02 -0300

    Update ProjectsSectionContent.jsx

--- src/Pages/PortfolioPage/pageContent/ProjectsSectionContent.jsx
+++ src/Pages/PortfolioPage/pageContent/ProjectsSectionContent.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { act } from 'react';
 import Card from '../../../Components/Cards/Card';
 import OldPortfolio from '../../../assets/img/OldPortfolio.png';
 import Slider from "react-slick";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import useWindowWidth from 'Hooks/useWindowWidth';
+import useWindowHeight from 'Hooks/useWindowHeight';
 
 const projects = [
     {
@@ -47,18 +48,51 @@ const icons = [
     },
 ];
 
-const settings = {
-    dots: true,
-    infinite: false,
-    arrows: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-
+const determineSlidesToShow = (width, height) => {
+    if (width >= 1200) {
+        // Pantallas grandes (1200px o más de ancho)
+        if (height >= 1080) {
+            return 3;
+        } else if (height > 900) {
+            return 3;
+        } else {
+            return 2;
+        }
+    } else if (width >= 900) {
+        // Pantallas medianas (900px - 1199px de ancho)
+        if (height >= 900) {
+            return 3;
+        } else if (height >= 600) {
+            return 1;
+        } else {
+            return 1;
+        }
+    } else {
+        // Pantallas pequeñas (menos de 900px de ancho)
+        if (height >= 900) {
+            return 1;
+        } else if (height >= 600) {
+            return 1;
+        } else {
+            return 1;
+        }
+    }
 };
 
 const ProjectsSectionContent = () => {
     const activeWidth = useWindowWidth();
+    const activeHeight = useWindowHeight();
+
+    const slidesToShow = determineSlidesToShow(activeWidth, activeHeight);
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        arrows: true,
+        speed: 500,
+        slidesToShow: slidesToShow,
+        slidesToScroll: 1,
+    };
 
     return (
         <section className="d-flex align-items-center justify-content-center h-100">
