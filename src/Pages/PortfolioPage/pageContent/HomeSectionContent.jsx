bd0b1c8bbe5cc0b5013a54a6c560219b424a4847
leandrosec19 <leandrosec19@gmail.com>
2024-10-21 17:00:16 +0000

    format

--- src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
+++ src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
@@ -5,9 +5,8 @@ import CVButton from "../Components/Button/CVButton";
 
 const HomeSectionContent = memo(() => {
     return (
-        <section
-            className={"d-flex flex-column h-100 align-items-center justify-content-center text-center"}
-        >
+        <section className={"d-flex flex-column h-100 align-items-center justify-content-center text-center"}>
+            
             <div data-aos="fade-down"><SocialMenu /></div>
             <div data-aos="zoom-in"><Title /></div>
             <div data-aos="fade-up"><CVButton /></div>
