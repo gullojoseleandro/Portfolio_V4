bd755410097c488a7d6721d7b34293cac45f18b9
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-28 20:55:57 -0300

    update: CVButton component

--- src/Pages/PortfolioPage/Components/Button/CVButton.jsx
+++ src/Pages/PortfolioPage/Components/Button/CVButton.jsx
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import CV from "./../../../../assets/Files/Gullo José Leandro - Octubre 2024.pdf";
 import "./CVButton.css"; 
 
-const CVButton = () => {
+const CVButton = ({...props}) => {
+    const { content, type, href, width, onClick } = props;
     const [activeButton, setActiveButton] = useState(false);
 
     return (
         <button
-            className={`cv-button ${activeButton ? 'active' : ''}`}
+            className={`cv-button w-100 ${activeButton ? 'active' : ''}`}
             onMouseEnter={() => setActiveButton(true)}
             onMouseLeave={() => setActiveButton(false)}
+            type={type}
+            width={width}
+            onClick={onClick}
         >
             <a 
-                href={CV} 
+                href={href ? {href}: null} 
                 target="_blank"
                 rel="noreferrer"
                 style={{ textDecoration: "none", color: "inherit" }}>
-                <p className="cv-button-text">Descargá mi CV</p>
+                <p className="cv-button-text">{content}</p>
             </a>
         </button>
     );
