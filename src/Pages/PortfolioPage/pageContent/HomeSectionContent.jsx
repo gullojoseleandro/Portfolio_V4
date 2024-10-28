9f595ef0f642790b70aafbdd650162981065e759
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-28 20:56:49 -0300

    Update HomeSectionContent.jsx

--- src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
+++ src/Pages/PortfolioPage/pageContent/HomeSectionContent.jsx
@@ -2,15 +2,16 @@ import React, { memo } from "react";
 import SocialMenu from "../../../Components/SocialMenu/SocialMenu";
 import Title from "../Components/Title/Title";
 import CVButton from "../Components/Button/CVButton";
+import TechnologiesBadge from "Components/Badges/TechnologiesBadge";
+import ContactsSectionContent from "./ContactsSectionContent";
 
-const HomeSectionContent = memo(() => {
+const HomeSectionContent = memo(({setActiveSection}) => {
     return (
         <section className={"d-flex flex-column h-100 align-items-center justify-content-center text-center"}>
-            
             <div data-aos="fade-down"><SocialMenu /></div>
             <div data-aos="zoom-in"><Title /></div>
-            <div data-aos="fade-up"><CVButton /></div>
-            
+            <div className={"py-5"} data-aos="zoom-in"><TechnologiesBadge /></div>
+            <div data-aos="fade-up"><CVButton onClick={()=>setActiveSection(<ContactsSectionContent />)} content="Contactá conmigo"/></div>
         </section>
     );
 });
