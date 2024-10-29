181b52216d0d1eb2f47d181c978bd2628a8cd884
Jose Leandro Gullo <leandro.gullo@dsv.com>
2024-10-29 10:25:21 -0300

    Update PortfolioPage.jsx

--- src/Pages/PortfolioPage/PortfolioPage.jsx
+++ src/Pages/PortfolioPage/PortfolioPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import fondoGalaxia from "../../assets/img/fondo_galaxia.gif";
 import PortfolioNavbar from "./Components/Navbar/jsx/PortfolioNavbar";
